3912a2425c5646492029cde2035a02ec27b4f474
guilherme guimaraes <143028145+GMG262000@users.noreply.github.com>
2024-10-17 20:47:46 -0300

    crud_basico

---
+++ crud_basico/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produto;
+use App\http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    public function create(){
+        return view('produto.create');
+    }
+    public function show($id){
+        $produto = Produto::findOrFail($id);
+        return view('produto.show',['produto'=>$produto]);
+
+    }
+    public function edit($id){
+        $produto = Produto::findOrFail($id);
+        return view('produto.edit',['produto'=> $produto]);
+    }
+    public function update(Request$request,$id){
+        $produto = Produto::findOrFail($id);
+        $produto -> update([
+            'nome' => $request->nome,
+            'custo' => $request->custo,
+            'preco' => $request->preco,
+            'quatidade' => $request->quantidade
+        ]);
+
+        return "Produto Atualizado com Sucesso!";
+    }
+    public function delete($id){
+        $produto = Produto::findOrFail($id);
+        return view('produto.delete',['produto'=> $produto]);
+    }
+    public function destroy($id){
+        $produto = Produto::findOrFail($id);
+        $produto -> delete();
+        return "Produto Excluido com Sucesso";
+    } 
+}
---
+++ crud_basico/resources/views/create.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Castrar um novo Produto</title>
+</head>
+<body>
+    <form action="">
+        <label  for=""> Nome Produto</label>
+        <input type="text" name="nome"><br/>
+        <label  for=""> Custo Produto</label>
+        <input type="text" name="custo"><br/>
+        <label  for=""> Preço Produto</label>
+        <input type="text" name="preço"><br/>
+        <label  for=""> Quantidade Produto</label>
+        <input type="text" name="quantidade"><br/>
+        <button>Salvar</button>
+    </form>
+    
+
+</body>
+</html>
